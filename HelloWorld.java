51d6b6dad94d9b2b3eb17d9d6091facd8cf01112
Abigail Pitcairn <abigail.pitcairn@maine.edu>
2025-02-10 14:39:33 -0500

    Added memory message feature. Closes #2

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class HelloWorld {
 
   public static void main (String[] args) {
+
+    Runtime runtime = Runtime.getRuntime();
   
     System.out.println("Hello world!");
 
@@ -14,6 +16,14 @@ public class HelloWorld {
     // https://www.geeksforgeeks.org/java-program-to-display-current-date-and-time/
     Date current_Date = new Date();
     System.out.println(current_Date);
+
+    // checking free memory:
+    // an approximation to the total amount of memory currently available for
+    // future allocated objects, measured in bytes.
+    long free_memory = runtime.freeMemory();
+    //printing free memory 
+    System.out.println("Free memory: " + free_memory + " bytes");
+
   
   }
 }
